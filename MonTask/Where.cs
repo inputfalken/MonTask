d37ab4352d69c34f298dc27dba02c5d0a5480a56
Robert Andersson <robert.andersson@offerta.se>
2017-08-17 13:01:36 +0200

    Add Where method extension

---
+++ MonTask/Where.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MonTask {
+    public static partial class Extensions {
+        public static async Task<TSource> Where<TSource>(this Task<TSource> task, Func<TSource, bool> predicate) {
+            if (task == null) throw new ArgumentNullException(nameof(task));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            var source = await task;
+            // TODO Install Option<T>
+            return predicate(source) ? source : default(TSource);
+        }
+    }
+}
